6cce956a3abc266d5c2e0396096a548900b061a8
中西誠 <nakanishimakoto@nakanishimakotonoMacBook-Air.local>
2022-05-06 09:52:04 +0900

    submit

---
+++ janken.rb
@@ -0,0 +1,85 @@
+puts "じゃんけんをしましょう。ジャンケン・・・・"
+
+def janken
+  puts "0（グー）1（チョキ）2（パー）3(戦わない）"
+
+  input_hand = gets.chomp.to_i
+  
+  puts "ホイ！"
+
+  jankens = ["グー","チョキ","パー"]
+
+  program_hand = jankens.sample
+  player_hand = jankens[input_hand]
+
+  puts "あなたの手:#{player_hand}"
+  puts "相手の手:#{program_hand}"
+
+  if player_hand == program_hand
+    puts "あいこで"
+    return true
+
+  elsif(player_hand == 0 && program_hand == 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
+    puts "あなたの勝ちです"
+    puts "あっち向いて〜"
+    puts "0（上）1（下）2（左）3(右）"
+
+    achimuite = ["上","下","左","右"]
+    
+    input_dir = gets.chomp.to_i
+    
+    puts "ホイ！"
+    
+    program_hand2 = achimuite.sample
+    player_hand2 = achimuite[input_dir]
+    
+    puts "あなたの手：#{achimuite[input_dir]}"
+    puts "相手の手：#{program_hand2}"
+    
+    if player_hand2 == program_hand2
+        puts "あなたの勝ちです"
+        return false
+    else
+        puts "もう一回" 
+        return true
+    end
+
+  else
+    puts "あなたの負けです"
+    puts "あっち向いて〜"
+    puts "0（上）1（下）2（左）3(右）"
+
+    achimuite = ["上","下","左","右"]
+    
+    input_dir = gets.chomp.to_i
+    
+    puts "ホイ！"
+    
+    program_hand2 = achimuite.sample
+    player_hand2 = achimuite[input_dir]
+    
+    puts "あなたの手：#{achimuite[input_dir]}"
+    puts "相手の手：#{program_hand2}"
+    
+    if player_hand2 == program_hand2
+        puts "あなたの負けです"
+        return false
+    else
+        puts "もう一回" 
+        return true
+    end
+  end
+end
+
+next_game = true
+
+while next_game do
+  next_game = janken
+end
+
+
+
+
+
+
+
