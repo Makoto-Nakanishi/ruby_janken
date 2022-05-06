d8452792585eff7f43268c223a2ee0b868373010
中西誠 <nakanishimakoto@nakanishimakotonoMacBook-Air.local>
2022-05-06 19:40:48 +0900

    submit

--- janken.rb
+++ janken.rb
@@ -1,28 +1,36 @@
+
 puts "じゃんけんをしましょう。ジャンケン・・・・"
 
 def janken
   puts "0（グー）1（チョキ）2（パー）3(戦わない）"
 
-  input_hand = gets.chomp.to_i
-
-  if input_hand == 3
+  begin
+   input_hand = gets.chomp
+   input_hand_n = Integer(input_hand)
+  rescue
+    puts "エラー！正しい数字を入力してください"
     return false
-  else 
-    puts "ジャンケンの手を入力してください"
-    return true
+  end
+
+  if input_hand_n == 3  
+    return false  
   end
   
   puts "ホイ！"
-
+  
   jankens = ["グー","チョキ","パー"]
-
+  
   program_hand = jankens.sample
-  player_hand = jankens[input_hand]
-
+  player_hand = jankens[input_hand_n]
+  
   puts "あなたの手:#{player_hand}"
   puts "相手の手:#{program_hand}"
+  
+  if player_hand == nil
+    puts "エラー！正しい数字を入力してください"
+    return false
 
-  if player_hand == program_hand
+  elsif player_hand == program_hand
     puts "あいこで"
     return true
 
@@ -32,19 +40,28 @@ def janken
     puts "0（上）1（下）2（左）3(右）"
 
     achimuite = ["上","下","左","右"]
-    
-    input_dir = gets.chomp.to_i
-    
+
+  begin
+    input_dir = gets.chomp
+    input_dir_n = Integer(input_dir)
+  rescue
+    puts "エラー！正しい数字を入力してください"
+    return false
+  end
+
     puts "ホイ！"
     
     program_hand2 = achimuite.sample
-    player_hand2 = achimuite[input_dir]
+    player_hand2 = achimuite[input_dir_n]
     
-    puts "あなたの手：#{achimuite[input_dir]}"
+    puts "あなたの手：#{player_hand2}"
     puts "相手の手：#{program_hand2}"
     
     if player_hand2 == program_hand2
         puts "あなたの勝ちです"
+        return false
+    elsif player_hand2 == nil
+        puts "エラー！正しい数字を入力してください"
         return false
     else
         puts "もう一回" 
@@ -58,18 +75,27 @@ def janken
 
     achimuite = ["上","下","左","右"]
     
-    input_dir = gets.chomp.to_i
+  begin
+    input_dir = gets.chomp
+    input_dir_n = Integer(input_dir)
+  rescue
+    puts "エラー！正しい数字を入力してください"
+    return false
+  end
     
     puts "ホイ！"
     
     program_hand2 = achimuite.sample
-    player_hand2 = achimuite[input_dir]
+    player_hand2 = achimuite[input_dir_n]
     
-    puts "あなたの手：#{achimuite[input_dir]}"
+    puts "あなたの手：#{player_hand2}"
     puts "相手の手：#{program_hand2}"
     
     if player_hand2 == program_hand2
         puts "あなたの負けです"
+        return false
+    elsif player_hand2 == nil
+        puts "エラー！正しい数字を入力してください"
         return false
     else
         puts "もう一回" 
